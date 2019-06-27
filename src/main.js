e4dafd2cd2c25716201eac919d18d1412d090112
jiangminxi <jiangminxi@banggood.com>
2019-06-27 15:20:30 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import {
-  Button
-} from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import './plugins/element.js'
 
-Vue.use(Button);
 Vue.config.productionTip = false
 
 new Vue({
